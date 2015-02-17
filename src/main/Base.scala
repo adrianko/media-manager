fcacce36dc1e581e13e011a8105071971d428437
Adrian Ko <adrianchko@gmail.com>
2015-02-17 10:02:10 +0000

    move function

--- src/main/Base.scala
+++ src/main/Base.scala
@@ -6,5 +6,12 @@ class Base {
         case Some(s) => s
         case None => ""
     }
+    
+    def checkOS(): Unit = {
+        if (!System.getProperty("os.name").contains("Windows")) {
+            System.out.println("This application is not designed to run on any operating system other than Windows. Sorry.")
+            System.exit(0)
+        }
+    }
 
 }
