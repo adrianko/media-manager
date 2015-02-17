c080ea4d66bcc1f812c8dadbedf5405365190593
Adrian Ko <adrianchko@gmail.com>
2015-02-17 10:02:35 +0000

    change var name

--- src/main/MediaManager.scala
+++ src/main/MediaManager.scala
@@ -33,17 +33,14 @@ object MediaManager extends Base {
     val keepList: String = basePath + ex(config.get("keep_list"))
 
     def main(args: Array[String]) {
-        if (!System.getProperty("os.name").contains("Windows")) {
-            System.out.println("This application is not designed to run on any operating system other than Windows. Sorry.")
-            System.exit(0)
-        }
+        checkOS()
         
-        val torrents: JSONArray = new JSONParser().parse(Downloader.getStatus).asInstanceOf[JSONObject].get("torrents")
+        val queue: JSONArray = new JSONParser().parse(Downloader.getStatus).asInstanceOf[JSONObject].get("torrents")
                 .asInstanceOf[JSONArray]
 
         //JSON array doesn't support foreach. Maybe use an iterator?
-        for (i: Int <- 0 to (torrents.size() - 1)) {
-            val t = torrents.get(i).asInstanceOf[JSONArray]
+        for (i: Int <- 0 to (queue.size() - 1)) {
+            val t = queue.get(i).asInstanceOf[JSONArray]
 
             if (t.get(21).toString.equals(Downloader.seedingMessage)) {
                 Downloader.clearSeed(t.get(0).toString)
