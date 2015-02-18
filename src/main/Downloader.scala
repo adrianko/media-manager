7d7487d5c77774d8c2f88c9333a57a2b0bc429e2
AK <adrianchko@gmail.com>
2015-02-18 21:18:34 +0000

    Use db config for ut settings

--- src/main/Downloader.scala
+++ src/main/Downloader.scala
@@ -6,20 +6,12 @@ import java.util.Base64
 
 import scala.io.Source._
 
-import main.MediaManager._
-
 object Downloader extends Base {
     
     val seedingMessage: String = "Seeding 100.0 %"
 
-    val ut: Map[String, String] = Map[String, String](
-        "user" -> "root",
-        "pass" -> ex(config.get("password")),
-        "win_host" -> "127.0.0.1",
-        "other_host" -> ex(config.get("ip"))
-    )
-    
-    def getURL(params: String): String = "http://" + ex(ut.get("win_host")) + ":8080/gui/?" + params +
+    def getURL(params: String): String =
+        "http://" + ex(MediaManager.settings.get("dl_host")) + ":" + ex(MediaManager.settings.get("dl_port")) + "/gui/?" + params +
         "&list=1&cid=0&getmsg=1&t=" + System.currentTimeMillis
     
     def getStatus: String = download(getURL("1=1"), content = true)
@@ -30,7 +22,7 @@ object Downloader extends Base {
         val con: HttpURLConnection  = new URL(url).openConnection().asInstanceOf[HttpURLConnection]
         con.setRequestMethod("GET")
         con.setRequestProperty("Authorization", "Basic " + new String(Base64.getEncoder.encode(
-            (ex(ut.get("user")) + ":" + ex(ut.get("pass"))).getBytes))
+            (ex(MediaManager.settings.get("dl_user")) + ":" + ex(MediaManager.settings.get("dl_pass"))).getBytes))
         )
 
         if (!content) {
