d1f019b9b6d1e3c8b0cd26709e6d25e289e820e0
AK <adrianchko@gmail.com>
2015-02-18 20:18:13 +0000

    Add getConnection

--- src/main/DB.scala
+++ src/main/DB.scala
@@ -1,5 +1,26 @@
 package main
 
+import java.io.File
+import java.sql.{DriverManager, Connection}
+
 object DB {
 
+    var conn: Connection = null
+
+    def getConnection: Connection = {
+        if (conn == null) {
+            try {
+                Class.forName("org.sqlite.JDBC")
+                conn = DriverManager.getConnection("jdbc:sqlite:" + new File(getClass.getResource(".").getFile).getAbsolutePath + "/../../../../db/shows.db")
+            } catch {
+                case e: Any => e.printStackTrace()
+            }
+        }
+
+        conn
+    }
+
+    def loadSettings(): Unit = {
+
+    }
 }
