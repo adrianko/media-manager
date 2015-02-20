f60dbd1c00818cb0b8caed39ce70540a0b70bdd4
Adrian Ko <adrianchko@gmail.com>
2015-02-20 16:58:25 +0000

    return anyway

--- src/main/Downloader.scala
+++ src/main/Downloader.scala
@@ -14,22 +14,18 @@ object Downloader extends Base {
         "http://" + ex(MediaManager.settings.get("dl_host")) + ":" + ex(MediaManager.settings.get("dl_port")) + "/gui/?" +
             params + "&list=1&cid=0&getmsg=1&t=" + System.currentTimeMillis
     
-    def getStatus: String = download(getURL("1=1"), content = true)
+    def getStatus: String = download(getURL("1=1"))
     
-    def sendAction(hash: String, action: String) = download(getURL("action=" + action + "&hash=" + hash), content = false)
+    def sendAction(hash: String, action: String) = download(getURL("action=" + action + "&hash=" + hash))
     
-    def download(url: String, content: Boolean): String = {
+    def download(url: String): String = {
         val con: HttpURLConnection  = new URL(url).openConnection().asInstanceOf[HttpURLConnection]
         con.setRequestMethod("GET")
         con.setRequestProperty("Authorization", "Basic " + new String(Base64.getEncoder.encode(
             (ex(MediaManager.settings.get("dl_user")) + ":" + ex(MediaManager.settings.get("dl_pass"))).getBytes))
         )
-
-        if (!content) {
-            con.getResponseCode.toString
-        } else {
-           fromInputStream(con.getContent.asInstanceOf[InputStream]).getLines().mkString
-        }
+        
+        fromInputStream(con.getContent.asInstanceOf[InputStream]).getLines().mkString
     }
 
     def clearSeed(hash: String): Unit = {
