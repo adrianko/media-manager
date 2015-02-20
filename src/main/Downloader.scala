d9569981d99605c0d17de906925896f0aab675d0
Adrian Ko <adrianchko@gmail.com>
2015-02-20 17:35:32 +0000

    move queue clearing into downloader

--- src/main/Downloader.scala
+++ src/main/Downloader.scala
@@ -38,5 +38,18 @@ object Downloader extends Base {
     }
     
     def getQueue: JSONArray = new JSONParser().parse(getStatus).asInstanceOf[JSONObject].get("torrents").asInstanceOf[JSONArray]
+    
+    def clearFinished(): Unit = {
+        val queue: JSONArray = Downloader.getQueue
+
+        //JSON array doesn't support foreach. Maybe use an iterator?
+        for (i: Int <- 0 to (queue.size() - 1)) {
+            val t = queue.get(i).asInstanceOf[JSONArray]
+
+            if (Downloader.complete(t.get(21).toString)) {
+                Downloader.clear(t.get(0).toString)
+            }
+        }
+    }
 
 }
--- src/main/MediaManager.scala
+++ src/main/MediaManager.scala
@@ -19,18 +19,8 @@ object MediaManager extends Base {
     }
     
     def MediaManager(): Unit = {
-        val queue: JSONArray = Downloader.getQueue
-        
-        //JSON array doesn't support foreach. Maybe use an iterator?
-        for (i: Int <- 0 to (queue.size() - 1)) {
-            val t = queue.get(i).asInstanceOf[JSONArray]
-
-            if (Downloader.complete(t.get(21).toString)) {
-                Downloader.clear(t.get(0).toString)
-            }
-        }
-
-        Manager.processFolder(Manager.fileList, DB.getKeepList).foreach{ f => println(f.getAbsoluteFile) }
+        Downloader.clearFinished()
+        Manager.processFolder().foreach{ f => println(f.getAbsoluteFile) }
     }
 
 }
