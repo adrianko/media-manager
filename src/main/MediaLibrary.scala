9f38362497a9d7699417efc2e3e8569c4c1964ec
AK <adrianchko@gmail.com>
2015-02-21 23:54:08 +0000

    Create refresh method for htpc lib

--- src/main/MediaLibrary.scala
+++ src/main/MediaLibrary.scala
@@ -2,8 +2,10 @@ package main
 
 import java.io.File
 
-object MediaLibrary {
+object MediaLibrary extends Base {
 
     def move(src: File): Unit = ()
 
+    def refresh(): Unit = Downloader.download("http://" + ex(MediaManager.settings.get("lib_host")) + ":" +
+        ex(MediaManager.settings.get("lib_port")) + ex(MediaManager.settings.get("lib_path")))
 }
