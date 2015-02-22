48bad78b5f9562eba2d622a9db34dfb6e169699c
AK <adrianchko@gmail.com>
2015-02-22 00:05:53 +0000

    Retrieve show Folders

--- src/main/MediaLibrary.scala
+++ src/main/MediaLibrary.scala
@@ -4,6 +4,14 @@ import java.io.File
 
 object MediaLibrary extends Base {
 
+    val mediaLibraryFolder: File = new File(ex(MediaManager.settings.get("lib_folder")))
+
+    val processingList = new File(ex(MediaManager.settings.get("processed_dir"))).listFiles.toList
+
+    def retrieveFiles(): Unit = {
+        val showFolders: List[File] = mediaLibraryFolder.listFiles.toList.filter(_.isDirectory)
+    }
+
     def move(src: File): Unit = ()
 
     def refresh(): Unit = Downloader.download("http://" + ex(MediaManager.settings.get("lib_host")) + ":" +
