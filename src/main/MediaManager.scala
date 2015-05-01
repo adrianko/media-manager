710c263c765cf1d3a6af9e0fb081342eb4419e9b
Adrian Ko <adrianchko@gmail.com>
2015-05-01 08:37:00 +0100

    Finish todos

--- src/main/MediaManager.scala
+++ src/main/MediaManager.scala
@@ -2,8 +2,6 @@ package main
 
 /**
  * MediaManager class
- * TODO Process files to be kept
- * TODO SD files move to sync and library
  */
 object MediaManager extends Base {
 
@@ -17,13 +15,7 @@ object MediaManager extends Base {
     
     def MediaManager(): Unit = {
         Downloader.clearFinished()
-        Manager.retrieveFiles().foreach{ f =>
-            if (f.getName.toLowerCase.contains("1080p")) {
-                Manager.move(f.getAbsoluteFile)
-            } else {
-                Manager.copy(f.getAbsoluteFile)
-            }
-        }
+        Manager.retrieveFiles().foreach{ f => Manager.move(f.getAbsoluteFile) }
         Manager.processFolder()
         Manager.cleanupFolder()
         MediaLibrary.retrieveFiles()
